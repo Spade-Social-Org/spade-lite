1d119a4941c0e81c99f6e44d763702010eea9721
supreme2580 <victoromorogbe69@gmail.com>
2024-01-05 15:13:58 +0100

    fixed-build-issue

--- test/spider_image_assets_test.dart
+++ test/spider_image_assets_test.dart
@@ -5,168 +5,162 @@ import 'package:spade_lite/resources/resources.dart';
 
 void main() {
   test('spider_image_assets assets test', () {
-    expect(File(SpiderImageAssets.beachPana1).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.camera).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.component11_).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.coupleCuate11).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.ellipse365).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.ellipse366).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.ellipse367).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.ellipse368).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.ellipse369).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.ellipse3721_).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.ellipse372).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.ellipse377).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.ellipse378).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.ellipse379).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.group949).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.groupDiscussionCuate1).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.hotel1).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.hotel2).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.image40).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.inLoveAmico1).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.inLovePana1).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.inLoveRafiki111_).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.inLoveRafiki11).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.locationIconUia).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.onlineDatingCuate1).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.path287).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.rectangle1595).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.rectangle1597).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.rectangle1598).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.rectangle33021_).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.rectangle3302).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.rectangle3381).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.rectangle3382).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.rectangle3383).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.rectangle3384).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.screenShot20220421At104111Am).existsSync(),
-        isTrue);
-    expect(File(SpiderImageAssets.screenShot20220421At104148Am).existsSync(),
-        isTrue);
-    expect(File(SpiderImageAssets.screenshot).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.union).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.vector2_).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.vector3_).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.vector4_).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.vector5_).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.vector).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.afternoon).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.analysisOptions).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.arrowforward).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.avatar).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.biGenderMale1_).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.biGenderMale).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.bottomNav).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.buildings2).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.cake).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.calendar).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.call).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.cardLight).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.chartGraphic).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.clubFill).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.club1).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.club2).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.club3).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.diamondFill).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.emptyMessage).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.evening).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.flipCard).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.freepikCharacter1Inject310).existsSync(),
-        isTrue);
-    expect(File(SpiderImageAssets.freepikCharacter1Inject316).existsSync(),
-        isTrue);
-    expect(File(SpiderImageAssets.freepikCharacter2Inject310).existsSync(),
-        isTrue);
-    expect(File(SpiderImageAssets.freepikCharacter2Inject316).existsSync(),
-        isTrue);
-    expect(
-        File(SpiderImageAssets.freepikCharacter2Inject45).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.freepikCharacter3Inject111_).existsSync(),
-        isTrue);
-    expect(File(SpiderImageAssets.freepikCharacter3Inject310).existsSync(),
-        isTrue);
-    expect(File(SpiderImageAssets.freepikCharacter3Inject316).existsSync(),
-        isTrue);
-    expect(File(SpiderImageAssets.freepikCharacter5Inject310).existsSync(),
-        isTrue);
-    expect(File(SpiderImageAssets.freepikCharacter5Inject316).existsSync(),
-        isTrue);
-    expect(File(SpiderImageAssets.friends).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.globalLight).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.global).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.grid).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.guide2).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.guide3).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.guide4).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.guide5).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.guide6).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.guide7).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.heartFill).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.heart).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.hearticon).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.heartz).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.iconParkOutlineVirgo).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.image).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.image1).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.leader1).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.listColored).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.list).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.location).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.location2).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.loveRound).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.man).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.matchImage).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.mdiBellNotificationOutline).existsSync(),
-        isTrue);
-    expect(File(SpiderImageAssets.mdiZodiacLeo).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.messageLight).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.message).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.mixed1).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.moreVert).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.morning).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.movie1).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.museum1).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.museum2).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.n1331529652065061).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.n3cards).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.n50501).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.no).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.onboarding).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.onboarding1).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.other).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.other2).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.people).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.perPie).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.personGroup).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.pexelsMwabonje18209191).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.placeimage).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.placeimage2).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.places).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.probaBg).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.probability).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.redNdYell).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.redAndYellowCardsSvgrepoCom1).existsSync(),
-        isTrue);
-    expect(File(SpiderImageAssets.restaurant).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.rightpan).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.ruler).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.search).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.send).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.sometimes).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.spadeFilled).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.spadeLight).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.spadeSmall).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.spade).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.supporter1).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.temp).existsSync(), isTrue);
-    expect(
-        File(SpiderImageAssets.temperatureSvgrepoCom31).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.timer).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.uLocationArrow).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.video).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.weight).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.woman).existsSync(), isTrue);
-    expect(File(SpiderImageAssets.yes).existsSync(), isTrue);
+    List<String> assetsToCheck = [
+      SpiderImageAssets.beachPana1,
+      SpiderImageAssets.camera,
+      SpiderImageAssets.component11_,
+      SpiderImageAssets.coupleCuate11,
+      SpiderImageAssets.ellipse365,
+      SpiderImageAssets.ellipse366,
+      SpiderImageAssets.ellipse367,
+      SpiderImageAssets.ellipse368,
+      SpiderImageAssets.ellipse369,
+      SpiderImageAssets.ellipse3721_,
+      SpiderImageAssets.ellipse372,
+      SpiderImageAssets.ellipse377,
+      SpiderImageAssets.ellipse378,
+      SpiderImageAssets.ellipse379,
+      SpiderImageAssets.group949,
+      SpiderImageAssets.groupDiscussionCuate1,
+      SpiderImageAssets.hotel1,
+      SpiderImageAssets.hotel2,
+      SpiderImageAssets.image40,
+      SpiderImageAssets.inLoveAmico1,
+      SpiderImageAssets.inLovePana1,
+      SpiderImageAssets.inLoveRafiki111_,
+      SpiderImageAssets.inLoveRafiki11,
+      SpiderImageAssets.locationIconUia,
+      SpiderImageAssets.onlineDatingCuate1,
+      SpiderImageAssets.path287,
+      SpiderImageAssets.rectangle1595,
+      SpiderImageAssets.rectangle1597,
+      SpiderImageAssets.rectangle1598,
+      SpiderImageAssets.rectangle33021_,
+      SpiderImageAssets.rectangle3302,
+      SpiderImageAssets.rectangle3381,
+      SpiderImageAssets.rectangle3382,
+      SpiderImageAssets.rectangle3383,
+      SpiderImageAssets.rectangle3384,
+      SpiderImageAssets.screenShot20220421At104111Am,
+      SpiderImageAssets.screenShot20220421At104148Am,
+      SpiderImageAssets.screenshot,
+      SpiderImageAssets.union,
+      SpiderImageAssets.vector2_,
+      SpiderImageAssets.vector3_,
+      SpiderImageAssets.vector4_,
+      SpiderImageAssets.vector5_,
+      SpiderImageAssets.vector,
+      SpiderImageAssets.afternoon,
+      SpiderImageAssets.analysisOptions,
+      SpiderImageAssets.arrowforward,
+      SpiderImageAssets.avatar,
+      SpiderImageAssets.biGenderMale1_,
+      SpiderImageAssets.biGenderMale,
+      SpiderImageAssets.bottomNav,
+      SpiderImageAssets.buildings2,
+      SpiderImageAssets.cake,
+      SpiderImageAssets.calendar,
+      SpiderImageAssets.call,
+      SpiderImageAssets.cardLight,
+      SpiderImageAssets.chartGraphic,
+      SpiderImageAssets.clubFill,
+      SpiderImageAssets.club1,
+      SpiderImageAssets.club2,
+      SpiderImageAssets.club3,
+      SpiderImageAssets.diamondFill,
+      SpiderImageAssets.emptyMessage,
+      SpiderImageAssets.evening,
+      SpiderImageAssets.flipCard,
+      SpiderImageAssets.freepikCharacter1Inject310,
+      SpiderImageAssets.freepikCharacter1Inject316,
+      SpiderImageAssets.freepikCharacter2Inject310,
+      SpiderImageAssets.freepikCharacter2Inject316,
+      SpiderImageAssets.freepikCharacter2Inject45,
+      SpiderImageAssets.freepikCharacter3Inject111_,
+      SpiderImageAssets.freepikCharacter3Inject310,
+      SpiderImageAssets.freepikCharacter3Inject316,
+      SpiderImageAssets.freepikCharacter5Inject310,
+      SpiderImageAssets.freepikCharacter5Inject316,
+      SpiderImageAssets.friends,
+      SpiderImageAssets.globalLight,
+      SpiderImageAssets.global,
+      SpiderImageAssets.grid,
+      SpiderImageAssets.guide2,
+      SpiderImageAssets.guide3,
+      SpiderImageAssets.guide4,
+      SpiderImageAssets.guide5,
+      SpiderImageAssets.guide6,
+      SpiderImageAssets.guide7,
+      SpiderImageAssets.heartFill,
+      SpiderImageAssets.heart,
+      SpiderImageAssets.hearticon,
+      SpiderImageAssets.heartz,
+      SpiderImageAssets.iconParkOutlineVirgo,
+      SpiderImageAssets.image,
+      SpiderImageAssets.image1,
+      SpiderImageAssets.leader1,
+      SpiderImageAssets.listColored,
+      SpiderImageAssets.list,
+      SpiderImageAssets.location,
+      SpiderImageAssets.location2,
+      SpiderImageAssets.loveRound,
+      SpiderImageAssets.man,
+      SpiderImageAssets.matchImage,
+      SpiderImageAssets.mdiBellNotificationOutline,
+      SpiderImageAssets.mdiZodiacLeo,
+      SpiderImageAssets.messageLight,
+      SpiderImageAssets.message,
+      SpiderImageAssets.mixed1,
+      SpiderImageAssets.moreVert,
+      SpiderImageAssets.morning,
+      SpiderImageAssets.movie1,
+      SpiderImageAssets.museum1,
+      SpiderImageAssets.museum2,
+      SpiderImageAssets.n1331529652065061,
+      SpiderImageAssets.n3cards,
+      SpiderImageAssets.n50501,
+      SpiderImageAssets.no,
+      SpiderImageAssets.onboarding,
+      SpiderImageAssets.onboarding1,
+      SpiderImageAssets.other,
+      SpiderImageAssets.other2,
+      SpiderImageAssets.people,
+      SpiderImageAssets.perPie,
+      SpiderImageAssets.personGroup,
+      SpiderImageAssets.pexelsMwabonje18209191,
+      SpiderImageAssets.placeimage,
+      SpiderImageAssets.placeimage2,
+      SpiderImageAssets.places,
+      SpiderImageAssets.probability,
+      SpiderImageAssets.redNdYell,
+      SpiderImageAssets.redAndYellowCardsSvgrepoCom1,
+      SpiderImageAssets.restaurant,
+      SpiderImageAssets.rightpan,
+      SpiderImageAssets.ruler,
+      SpiderImageAssets.search,
+      SpiderImageAssets.send,
+      SpiderImageAssets.sometimes,
+      SpiderImageAssets.spadeFilled,
+      SpiderImageAssets.spadeLight,
+      SpiderImageAssets.spadeSmall,
+      SpiderImageAssets.spade,
+      SpiderImageAssets.supporter1,
+      SpiderImageAssets.temp,
+      SpiderImageAssets.temperatureSvgrepoCom31,
+      SpiderImageAssets.timer,
+      SpiderImageAssets.uLocationArrow,
+      SpiderImageAssets.video,
+      SpiderImageAssets.weight,
+      SpiderImageAssets.woman,
+      SpiderImageAssets.yes,
+    ];
+
+    for (String asset in assetsToCheck) {
+      try {
+        expect(File(asset).existsSync(), isTrue);
+      } catch (e) {
+        print('Asset not found: $asset');
+      }
+    }
   });
 }
